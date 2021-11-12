00d80ce5e48fa0a37048b9f6cc5580fa9aee04b5
RobertEli <57449733+RobertElikplim@users.noreply.github.com>
2021-11-11 18:53:21 -0600

    update

--- frontend/bread-of-life/server.js
+++ frontend/bread-of-life/server.js
@@ -3,5 +3,8 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, `/dist/index.html`))
+    })
+const port = process.env.PORT || 8080;
 app.listen(port);
